b72bd1cab0ac75606517a817af0882087cb0e71f
Parin <ptimbadi@purdue.edu>
2025-09-17 19:00:11 -0400

    working with message handling, almost done with next_message_id

--- rtl/moldUDP_64.sv
+++ rtl/moldUDP_64.sv
@@ -18,6 +18,14 @@ module moldUDP#(
     typedef enum logic [2:0] {IDLE, CHKSUM, SESSION, MSG, REREQ1, REREQ2, REREQ3, PAYLOAD} state; 
 
     logic miss;
+    logic end_of_session;
+    logic [15:0] checksum;
+    logic [79:0] session;
+    logic [63:0] sequence_num;
+    logic [63:0] expected_sequence_num;
+    logic [63:0] next_expected_sequence_num;
+    logic [15:0] message_count;
+    logic [15:0] message_length;
     state curr_state, next_state;
 
 
@@ -33,8 +41,31 @@ module moldUDP#(
 
     always_comb begin
 
-    // next state logic 
+    // output logic
+    mold_valid = (state == PAYLOAD);
+    mold_request_valid = (state == REREQ1 | state == REREQ2 | state == REREQ3);
 
+
+    // handling inputs from UDP layer
+    if (state == CHKSUM) begin
+        checksum = UDP_payload[15:0];
+	session[79:32] = UDP_payload[63:16];
+    end else if (state == SESSION) begin
+	session[31:0] = UDP_payload[31:0];
+	sequence_num[63:32] = UDP_payload[63:32];
+    end else if (state == MSG) begin 
+	sequence_num[31:0] = UDP_payload[31:0];
+	message_count = UDP_payload[47:32];
+	message_length = UDP_payload[63:47];
+    end
+
+    end_of_session = (message_length == 16'hffff);
+
+    // handling misses
+    miss = (sequence_num != expected_sequence_num && message_length == 16'b0);
+    next_expected_sequence_num = (message_length == 16'b0 ? 64'b1 : sequence_num + 1);
+
+    // next state logic 
     case (curr_state) begin
 	IDLE: next_state = CHKSUM;
         CHKSUM: next_state = SESSION;
@@ -46,11 +77,4 @@ module moldUDP#(
 	PAYLOAD: next_state = IDLE;
     endcase 
 
-
-
-    // output logic
-    mold_valid = (state == PAYLOAD);
-    mold_request_valid = (state == REREQ1 | state == REREQ2 | state == REREQ3);
-
-    
     end
