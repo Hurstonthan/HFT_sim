cb7102aeb5d460982c2a9d59fec79e15f002c5c5
Parin Timbadia <pptimbadia@gmail.com>
2025-09-22 16:37:00 -0400

    rerequest added, state bugfixes, typo bugfix

--- rtl/moldUDP_64.sv
+++ rtl/moldUDP_64.sv
@@ -12,7 +12,7 @@ module moldUDP#(
     output logic [15:0] mold_length,
     output logic [63:0] mold_payload,
     output logic mold_request_valid, 
-    output logic [63:0] mold_request_valid
+    output logic [63:0] mold_request_payload
 );
 
     typedef enum logic [2:0] {IDLE, CHKSUM, SESSION, MSG, REREQ1, REREQ2, REREQ3, PAYLOAD} state; 
@@ -26,6 +26,10 @@ module moldUDP#(
     logic [63:0] next_expected_sequence_num;
     logic [15:0] message_count;
     logic [15:0] message_length;
+    logic [79:0] request_session;
+    logic [63:0] request_sequence_num;
+    logic [15:0] request_message_count;
+    logic[63:0] miss_count;
     state curr_state, next_state;
 
 
@@ -42,33 +46,41 @@ module moldUDP#(
 
 
     always_comb begin
-    
         // output logic
-        mold_valid = (state == PAYLOAD);
-        mold_request_valid = (state == REREQ1 | state == REREQ2 | state == REREQ3);
+        mold_valid = (curr_state == PAYLOAD);
+        mold_request_valid = (curr_state == REREQ1 | curr_state == REREQ2 | curr_state == REREQ3);
     
         // is this a heartbeat message to end session?
         end_of_session = (message_length == end_session);
     
     
         // handling inputs from UDP layer
-        if (state == CHKSUM) begin
+        if (curr_state == CHKSUM) begin
             checksum = UDP_payload[15:0];
             session[79:32] = UDP_payload[63:16];
-        end else if (state == SESSION) begin
+        end else if (curr_state == SESSION) begin
             session[31:0] = UDP_payload[31:0];
             sequence_num[63:32] = UDP_payload[63:32];
-        end else if (state == MSG) begin 
+        end else if (curr_state == MSG) begin 
             sequence_num[31:0] = UDP_payload[31:0];
             message_count = UDP_payload[47:32];
             message_length = UDP_payload[63:47];
-        end else if (state == PAYLOAD && message_length != heartbeat && message_length != end_session) begin 
-    	    mold_payload = UDP_payload;	
-        end
+        end else if (curr_state == PAYLOAD && message_length != heartbeat && message_length != end_session) begin 
+            mold_payload = UDP_payload;	
+        end else if (curr_state == REREQ1) begin
+	    request_session[79:16] = session[79:16]; 
+        end else if (curr_state == REREQ2) begin
+	    request_session[15:0] = session[15:0];
+	    request_sequence_num[63:16] = sequence_num[63:16]; 
+        end else if (curr_state == REREQ3) begin
+	    request_sequence_num[15:0] = sequence_num[15:0]; 
+	    request_message_count = miss_count[15:0];
+	end
     
     
         // handling misses
         miss = (sequence_num != expected_sequence_num && message_length != heartbeat && message_length != end_session);
+	miss_count = sequence_num - expected_sequence_num;
         if (end_of_session) begin
             next_expected_sequence_num = 64'b1;
         end else if (message_length == heartbeat) begin 
